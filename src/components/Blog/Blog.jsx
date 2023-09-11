e2ee0239e4197841f0ad917edb7ecb38e2830995
Ateka Sultana <atekaoishi78@gmail.com>
2023-09-12 03:57:51 +0600

    commited

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,12 +1,13 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import { FaBookmark } from 'react-icons/fa';
 
-const Blog = ({blog}) => {
+const Blog = ({blog, handleAddToBookmark, handleMarkAsRead}) => {
     // console.log(blog);
     const {title, cover, author, author_img, posted_date, reading_time, hashtags} = blog;
     return (
-        <div>
-            <img src={cover} alt={`Cover picture of the title ${title}`} />
-            <div className='flex justify-between'>
+        <div className='mb-20 space-y-4'>
+            <img className='w-full mb-8' src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between mb-4'>
                 <div className='flex'>
                     <img className='w-14' src={author_img} alt="" />
                     <div className='ml-6'>
@@ -16,6 +17,10 @@ const Blog = ({blog}) => {
                 </div>
                 <div>
                     <span>{reading_time}min read</span>
+                    <button
+                     onClick={() => handleAddToBookmark(blog)}
+                       className='ml-2 text-2xl text-red-600'
+                     ><FaBookmark>Bookmark</FaBookmark></button>
 
                 </div>
             </div>
@@ -25,13 +30,20 @@ const Blog = ({blog}) => {
                    hashtags.map((hash, idx) => <span key={idx}><a href="">#{hash}</a></span>)
                 }
             </p>
+
+            <button
+            onClick={ () =>handleMarkAsRead(reading_time)}
+             className='text-purple-800 font-bold underline'
+            >Mark As Read</button>
             
         </div>
     );
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired
+    blog: PropTypes.object.isRequired,
+    handleAddToBookmark: PropTypes.func,
+    handleMarkAsRead: PropTypes.func
 }
 
 export default Blog;
